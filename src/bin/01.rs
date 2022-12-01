9b579e32ae0a5d03ca1a677aa784bb75a3debf69
Max Glass <glassmax13@gmail.com>
2022-12-01 17:53:40 -0500

    Use iterator `max()` function for day 1 part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,20 +1,14 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let inputs = String::from_utf8(std::fs::read("inputs/01.txt")?)?;
 
-    let calorie_sums: Vec<usize> = inputs
+    let largest = inputs
         .split("\n\n")
         .map(|elf| {
             elf.split('\n')
                 .fold(0, |acc, v| acc + v.parse::<usize>().unwrap_or(0))
         })
-        .collect();
-
-    let mut largest = 0;
-    for sum in calorie_sums {
-        if sum > largest {
-            largest = sum
-        }
-    }
+        .max()
+        .unwrap();
 
     println!("largest: {}", largest);
 
